7b7ddd2012cc4adae772eb25f8d85d8aa32d3180
luzagurto22 <luzemeritaagurtocruz@gmail.com>
2024-09-04 20:01:05 -0500

    Pagina en blanco

---
+++ src/Page/PaginaPrincipal.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+function PaginaPrincipal() {
+    return (
+        <div className="">
+            
+        </div>
+    );
+}
+
+export default PaginaPrincipal;
